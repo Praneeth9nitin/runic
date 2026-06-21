d02c851f8cb1adfd03df49d208118eb9b50ae005
Praneeth gupta <nitinkumariit4@gmail.com>
2026-06-21 10:30:47 +0000

    v1 completed successfully changes hostname

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod container;
 mod namespace;
+use std::env;
 fn main() {
+    let args: Vec<String> = env::args().collect();
     let mut c = container::Container::new("cont1".to_string());
-    c.run("/bin/bash").unwrap();
+    c.run(&args[1]).unwrap();
     let code = c.wait().unwrap();
     println!("exited with code {code}");
 }
--- src/namespace.rs
+++ src/namespace.rs
@@ -1,13 +1,9 @@
-#[cfg(target_os = "linux")]
 use nix::sched::{CloneFlags, unshare};
 use nix::unistd::sethostname;
 
 pub fn set_namespace() -> anyhow::Result<()> {
     println!("setting up namespaces...");
-    #[cfg(target_os = "linux")]
-    {
-        unshare(CloneFlags::CLONE_NEWUTS)?;
-    }
+    unshare(CloneFlags::CLONE_NEWUTS)?;
     sethostname("runic-container")?;
     println!("namespaces done");
     Ok(())
