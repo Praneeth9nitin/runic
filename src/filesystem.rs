4c29bea426da04ee2a4a1d4c951ce6a8f16608d8
Praneeth gupta <nitinkumariit4@gmail.com>
2026-06-22 12:38:42 +0000

    v2 filesystem added

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -3,16 +3,16 @@ use std::fs::create_dir_all;
 use nix::unistd::pivot_root;
 
 pub fn set_filesystem(container_id: &str) -> anyhow::Result<()>{
-    create_dir_all(format!("/workspaces/runic/rootfs/{}/upper", container_id))?;
-    create_dir_all(format!("/workspaces/runic/rootfs/{}/work", container_id))?;
-    create_dir_all(format!("/workspaces/runic/rootfs/{}/merged", container_id))?;
-    create_dir_all(format!("/workspaces/runic/rootfs/{}/merged/oldroot", container_id))?;
-    let target = format!("/workspaces/runic/rootfs/{}/merged",container_id);
-    let old_root = format!("/workspaces/runic/rootfs/{}/merged/oldroot", container_id);
+    create_dir_all(format!("/home/ubuntu/runic/rootfs/{}/upper", container_id))?;
+    create_dir_all(format!("/home/ubuntu/runic/rootfs/{}/work", container_id))?;
+    create_dir_all(format!("/home/ubuntu/runic/rootfs/{}/merged", container_id))?;
+    create_dir_all(format!("/home/ubuntu/runic/rootfs/{}/merged/oldroot", container_id))?;
+    let target = format!("/home/ubuntu/runic/rootfs/{}/merged",container_id);
+    let old_root = format!("/home/ubuntu/runic/rootfs/{}/merged/oldroot", container_id);
 
-     let options = format!("lowerdir=/workspaces/runic/rootfs/base,\
-                   upperdir=/workspaces/runic/rootfs/{}/upper,
-                   workdir=/workspaces/runic/rootfs/{}/work", container_id, container_id);
+     let options = format!("lowerdir=/home/ubuntu/runic/rootfs/base,\
+                   upperdir=/home/ubuntu/runic/rootfs/{}/upper,
+                   workdir=/home/ubuntu/runic/rootfs/{}/work", container_id, container_id);
     mount(Some("overlay"), target.as_str(), Some("overlay"), MsFlags::empty(), Some(options.as_str()))?;
     println!("mount 1 done");
     pivot_root(target.as_str(), old_root.as_str())?;
