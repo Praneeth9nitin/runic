94d63a8a7294b60be2e064cf2c6e734faa466c2f
praneeth9nitin <nitinkumariit4@gmail.com>
2026-06-24 12:56:51 +0530

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 89c5b832cf870217a8fe433c58162536b8023dcf, reversing
    changes made to cfd0031b6beb0d7b900f9cb7b2f605c1223124d6.

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -3,22 +3,37 @@ use std::fs::create_dir_all;
 use nix::unistd::pivot_root;
 
 pub fn set_filesystem(container_id: &str) -> anyhow::Result<()>{
-    create_dir_all(format!("/home/ubuntu/runic/rootfs/{}/upper", container_id))?;
-    create_dir_all(format!("/home/ubuntu/runic/rootfs/{}/work", container_id))?;
-    create_dir_all(format!("/home/ubuntu/runic/rootfs/{}/merged", container_id))?;
-    create_dir_all(format!("/home/ubuntu/runic/rootfs/{}/merged/oldroot", container_id))?;
-    let target = format!("/home/ubuntu/runic/rootfs/{}/merged",container_id);
-    let old_root = format!("/home/ubuntu/runic/rootfs/{}/merged/oldroot", container_id);
+    // if base doesn't exist in /tmp, copy from permanent location
+    if !std::path::Path::new("/tmp/runic/base/bin").exists() {
+        std::fs::create_dir_all("/tmp/runic/base")?;
+        // copy from permanent location
+        std::process::Command::new("cp")
+            .args(["-a", "/home/ubuntu/runic-base/.", "/tmp/runic/base/"])
+            .status()?;
+    }
+    mount(None::<&str>, "/", None::<&str>, MsFlags::MS_PRIVATE | MsFlags::MS_REC, None::<&str>)?;
+    println!("private mount done");
+    create_dir_all(format!("/tmp/runic/{}/upper", container_id))?;
+    create_dir_all(format!("/tmp/runic/{}/work", container_id))?;
+    create_dir_all(format!("/tmp/runic/{}/merged", container_id))?;
+    create_dir_all(format!("/tmp/runic/{}/merged/oldroot", container_id))?;
+    let target = format!("/tmp/runic/{}/merged",container_id);
+    let old_root = format!("/tmp/runic/{}/merged/oldroot", container_id);
 
-     let options = format!("lowerdir=/home/ubuntu/runic/rootfs/base,upperdir=/home/ubuntu/runic/rootfs/git {}/upper,workdir=/home/ubuntu/runic/rootfs/{}/work", container_id, container_id);
+    let options = format!("lowerdir=/tmp/runic/base,upperdir=/tmp/runic/{}/upper,workdir=/tmp/runic/{}/work", container_id, container_id);
+    println!("options: {}", options);
     mount(Some("overlay"), target.as_str(), Some("overlay"), MsFlags::empty(), Some(options.as_str()))?;
     println!("mount 1 done");
-    pivot_root(target.as_str(), old_root.as_str())?;
+    mount(Some(target.as_str()), target.as_str(), None::<&str>, MsFlags::MS_BIND | MsFlags::MS_REC, None::<&str>)?;
+    println!("bind mount done");
+    std::env::set_current_dir(&target)?;
+    println!("chdir done");
+    pivot_root(".", "./oldroot")?;
     println!("pivot done");
     std::env::set_current_dir("/")?;
     umount2("/oldroot", MntFlags::MNT_DETACH)?;
     println!("Unmount done");
     mount(Some("proc"), "/proc", Some("proc"), MsFlags::empty(), None::<&str>)?;
-    println!("mount 3 done");
+    println!("mount 2 done");
     Ok(())
 }
